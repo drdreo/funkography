72c0393c4303c278b20581f6e687a0105f501cf2
andreas@andreas-hahn.at <andreas@andreas-hahn.at>
2020-08-14 12:29:49 +0200

    First commit

---
+++ components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="navbar">
+    <a>Funky</a>
+    <a>works</a>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({});
+</script>
+
+<style>
+.navbar {
+  position: absolute;
+  top: 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <Navbar/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+// https://minetta-demo-de.squarespace.com/?nochrome=true do like this
+export default Vue.extend({});
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
