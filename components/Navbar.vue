dbdc0c49d866084624625bef15cddc5a080ec639
andreas@andreas-hahn.at <andreas@andreas-hahn.at>
2020-08-14 13:08:58 +0200

    added dev images added scss variables.scss

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,15 @@
 <template>
   <header class="navbar">
-    <a>Funky</a>
-    <a>works</a>
+    <div class="navbar__brand">
+      Funky Funkwalter
+    </div>
+    <nav class="navbar__links">
+      <a class="navbar__links-entry">Work</a>
+      <a class="navbar__links-entry">About</a>
+      <a class="navbar__links-entry">Contact</a>
+      <a class="navbar__links-entry">Insta</a>
+    </nav>
+
   </header>
 </template>
 
@@ -11,9 +19,27 @@ import Vue from 'vue';
 export default Vue.extend({});
 </script>
 
-<style>
+<style lang="scss">
 .navbar {
   position: absolute;
   top: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  padding: var(--spacing-md);
+  font-size: var(--navbar-font-size);
+
+  &__brand {
+    font-size: var(--navbar-brand-font-size);
+  }
+
+  &__links {
+    &-entry:not(:last-child){
+      margin-right: var(--spacing-sm);
+    }
+  }
 }
+
+
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="container">
     <Navbar/>
+
+    <div class="photos-container">
+      <figure class="photo" v-for="photo in photos">
+        <i style="padding-bottom:150%"></i>
+        <img :src="photo"/>
+      </figure>
+    </div>
   </div>
 </template>
 
@@ -8,16 +15,66 @@
 import Vue from 'vue';
 
 // https://minetta-demo-de.squarespace.com/?nochrome=true do like this
-export default Vue.extend({});
+export default Vue.extend({
+  data() {
+    return {
+      photos: [
+        'https://www.esa.int/var/esa/storage/images/19716864-11-eng-GB/ESA_root_pillars.jpg',
+        'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/andromeda-galaxy-royalty-free-image-1585682435.jpg',
+        'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2F765877054%2F960x0.jpg%3Ffit%3Dscale',
+        'https://scitechdaily.com/images/Illustration-Photons-Galaxy.jpg',
+        'https://specials-images.forbesimg.com/imageserve/1176555906/960x0.jpg?fit=scale',
+        'https://orsted.com/-/media/WWW/Images/Corp/Campaign/SpaceSafari/space-safari-background.png',
+        'https://spacenews.com/wp-content/uploads/2018/05/24359364107_152b0152ff_k.jpg',
+        'https://scx2.b-cdn.net/gfx/news/hires/2019/4-space.jpg',
+      ],
+    };
+  },
+
+});
 </script>
 
-<style>
+<style lang="scss">
 .container {
   margin: 0 auto;
+  padding-top: var(--spacing-xl);
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
+
+  background-color: black;
+  color: white;
+}
+
+.photos-container {
+  padding: var(--spacing-md);
+  display: flex;
+  flex-wrap: wrap;
+
+
+  &::after {
+    content: '';
+    flex-grow: 999999999;
+  }
+
+  .photo {
+    margin: 2px;
+    background-color: violet;
+    position: relative;
+
+    i {
+      display: block;
+    }
+
+    img {
+      position: absolute;
+      top: 0;
+      width: 100%;
+      vertical-align: bottom;
+    }
+  }
+
 }
 </style>
